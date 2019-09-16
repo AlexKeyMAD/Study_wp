8ab930684a3510e7c000cda79a79a0f7c5b56466
Alexandr <it_np@mail.ru>
2019-09-16 19:00:34 +0600

    038

--- less_003_JS/js-2.js
+++ less_003_JS/js-2.js
@@ -9,3 +9,12 @@ document.getElementById('create-text').onclick = function() {
     new_p.innerHTML = 'Новый элемент';
     document.body.append(new_p);
 }
+
+document.getElementById('text-color').onclick = function() {
+    var elem_p = document.getElementsByTagName('p');
+
+    for (let index = 0; index < elem_p.length; index++) {
+        elem_p[index].style.color = 'red';
+
+    }
+}
