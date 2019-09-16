31c77fc6b163da1c75788de0b148891bf86bc2c6
Alexandr <it_np@mail.ru>
2019-09-16 17:51:26 +0600

    037

--- less_003_JS/js-2.js
+++ less_003_JS/js-2.js
@@ -1,3 +1,11 @@
 alert('Страница загружена!!!');
 
-document.getElementById('text').innerHTML = 'Добрый день';
+document.getElementById('button-change').onclick = function() {
+    document.getElementById('text').innerHTML = 'Добрый день';
+}
+
+document.getElementById('create-text').onclick = function() {
+    var new_p = document.createElement('p');
+    new_p.innerHTML = 'Новый элемент';
+    document.body.append(new_p);
+}
