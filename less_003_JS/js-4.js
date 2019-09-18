05256c791cb5f55fd611dc71c13415b0ba55a940
Alexandr <it_np@mail.ru>
2019-09-18 15:03:44 +0600

    043

--- less_003_JS/js-4.js
+++ less_003_JS/js-4.js
@@ -4,11 +4,10 @@ buttonCheckMagicWord.onclick = function() {
     let magicWord = 'Спасибо',
         inputMagicWord = document.getElementById('magic-word').value;
 
-    if (magicWord == inputMagicWord) {
+    if (magicWord.toLowerCase() == inputMagicWord.toLowerCase()) {
         alert('Вы правильно назвали "волшебное слово"!!!');
     } else {
         alert('Вы ошиблись, "волшебное слово" набрано не верно!!!');
     }
-
 
 }
