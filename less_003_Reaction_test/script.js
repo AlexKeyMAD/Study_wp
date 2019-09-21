f7543f7dd1c5aa69f35e0e05c6b17af77fd9d0e0
Alexandr Gussakov <it_np@mail.ru>
2019-09-21 15:06:29 +0600

    H_009

--- less_003_Reaction_test/script.js
+++ less_003_Reaction_test/script.js
@@ -1,1 +1,21 @@
 let reactionTime = document.getElementById('reaction-time');
+let startTime = new Date;
+
+function newRandomGeometricShape() {
+    //let currentTime = (new Date).getTime();
+    //
+    let divRandom = document.getElementById('box');
+
+    newElement = document.createElement('div');
+    newElement.onclick = function() {
+        alert('OK');
+    }
+    divRandom.append(newElement);
+
+    //
+    startTime = startTime.getTime();
+}
+
+document.getElementById('start').onclick = function() {
+    newRandomGeometricShape();
+}
