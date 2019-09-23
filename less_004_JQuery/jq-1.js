bbfebe0c91e61a5207bb6b93b189531af3279b95
Alexandr Gussakov <it_np@mail.ru>
2019-09-23 18:11:52 +0600

    H_015

--- less_004_JQuery/jq-1.js
+++ less_004_JQuery/jq-1.js
@@ -1,7 +1,14 @@
 $("#circle").click(function() {
-    $('p').html('Нажали круг');
+    $('#p1').html('Нажали круг');
+    $('#circle').css("background-color", "red");
 });
 
 $(".square").click(function() {
     $('iframe').attr('src', 'https://www.blizzard.com/ru-ru/');
+});
+
+alert($('body').css('width'));
+
+$('div').click(function() {
+    $(this).css('display', 'none');
 });
