17648c03fd8336192f899b5251628e81c5c2fc06
Alexandr <it_np@mail.ru>
2019-09-27 09:57:18 +0600

    049

--- less_004_JQuery/jq-4.js
+++ less_004_JQuery/jq-4.js
@@ -7,3 +7,6 @@ $("#green-sq").droppable({
         console.log(event);
     }
 });
+$("#accordion").accordion({ axis: "y", clearStyle: "button", fillSpace: true, collapsible: true });
+$("#sortable").sortable();
+$("#sortable").disableSelection();
