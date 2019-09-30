2c22f20480c2ce9286d21085c80e9d86e2e5cfff
Alexandr <it_np@mail.ru>
2019-09-30 12:44:28 +0600

    051

--- less_004_Code_Player/scripts/script.js
+++ less_004_Code_Player/scripts/script.js
@@ -1,10 +1,33 @@
 let visibleAreas = 2;
-$('#div-css').css('display', 'none');
-$('#div-js').css('display', 'none');
+$('#b-html').prop('checked', true);
+$('#b-result').prop('checked', true);
 
 function styleWorkspace() {
     widthPer = 100 / visibleAreas - 1;
     $('.workspace').css('width', String(widthPer) + '%');
+
+    for (let item of $('.butt')) {
+        if (item.checked) {
+            strVis = 'block';
+        } else {
+            strVis = 'none';
+        }
+        $('#' + item.name.replace('b-', 'div-')).css('display', strVis);
+    }
+
 }
 
 styleWorkspace();
+
+$(".butt").checkboxradio({
+    icon: false
+});
+
+$('.butt').on('change', function() {
+    if (this.checked) {
+        visibleAreas += 1;
+    } else {
+        visibleAreas -= 1;
+    }
+    styleWorkspace();
+});
