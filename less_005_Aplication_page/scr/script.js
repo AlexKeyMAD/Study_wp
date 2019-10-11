fb250a2ddad3a169c1f3d7a1113ed886f86c6fc6
Alexandr <it_np@mail.ru>
2019-10-11 21:42:13 +0600

    065

--- less_005_Aplication_page/scr/script.js
+++ less_005_Aplication_page/scr/script.js
@@ -1,4 +1,4 @@
 $('body').scrollspy({ target: '#navbar-example' });
 $('[data-spy="scroll"]').each(function() {
-    var $spy = $(this).scrollspy('refresh')
+    let $spy = $(this).scrollspy('refresh');
 });
