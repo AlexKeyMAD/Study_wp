28a133a4e649580434c4b0f7fad3c27542512e38
Alexandr <it_np@mail.ru>
2019-10-21 13:51:29 +0600

    071

--- less_006_php/index.php
+++ less_006_php/index.php
@@ -1,3 +1,13 @@
 <?php
-ehco "Hello word";
+$text = "hello";
+echo "<p>$text</p>";
+
+$name = 'Alex';
+echo '<p>меня зовут ';
+echo $name."</p><p>".$text."</p>";
+
+$arrayMy = array('Alex', 'Lena');
+
+print_r($arrayMy);
+
 ?>
