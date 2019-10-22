e131f469cd43ed3d675609e73d798e4a370eb425
Alexandr <it_np@mail.ru>
2019-10-22 14:42:41 +0600

    075

--- less_006_php/index-4.php
+++ less_006_php/index-4.php
@@ -9,19 +9,30 @@
     </form>
 </html>
 <?php
-    $endNumber = $_GET['endNumber'];
-    for ($i=1; $i < $endNumber; $i = $i + 2) { 
-        
-        $num = 0;
 
-        for ($t=1; $t <= $i; $t++) { 
-            if ($i % $t == 0) {
-                $num++;
+    if ($_GET) {
+        $endNumber = $_GET['endNumber'];
+        $strNum = 0;
+        $sim ='';
+        for ($i=1; $i < $endNumber; $i = $i + 2) { 
+            
+            $num = 0;
+
+            for ($t=1; $t <= $i; $t++) { 
+                if ($i % $t == 0) {
+                    $num++;
+                }
             }
-        }
 
-        if ($num < 3) {
-            echo $i."<br>";
+            if ($num < 3) {
+                $strNum++;
+                if ($strNum % 5 == 0) {
+                    $sim = "<br>";
+                }else {
+                    $sim = '&nbsp;&nbsp;&nbsp;&nbsp';
+                }
+                echo $i.$sim;
+            }
         }
     }
 ?>
