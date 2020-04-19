bb2c9c84676424db94192ff7b715fd65edab4b22
Alexandr Gussakov <it_np@mail.ru>
2020-04-19 20:41:56 +0600

    200419_2041

--- less_Home_Library/files/js/menu.js
+++ less_Home_Library/files/js/menu.js
@@ -1,31 +1,31 @@
 window.addEventListener('DOMContentLoaded', function() {
+    /*
+        let btnPy = document.getElementById('btn-python'),
+            btnJS = document.getElementById('btn-js'),
+            btn1c = document.getElementById('btn-1c'),
+            ulPy = document.getElementById('ul-python'),
+            ulJS = document.getElementById('ul-js'),
+            ul1c = document.getElementById('ul-1c');
 
-    let btnPy = document.getElementById('btn-python'),
-        btnJS = document.getElementById('btn-js'),
-        btn1c = document.getElementById('btn-1c'),
-        ulPy = document.getElementById('ul-python'),
-        ulJS = document.getElementById('ul-js'),
-        ul1c = document.getElementById('ul-1c');
+        function hideElementsMenu() {
+            ulPy.style.display = 'none';
+            ulJS.style.display = 'none';
+            ul1c.style.display = 'none';
+        }
 
-    function hideElementsMenu() {
-        ulPy.hidden = false;
-        ulJS.hidden = false;
-        ul1c.hidden = false;
-    }
+        btnPy.onclick = function() {
+            hideElementsMenu();
+            ulPy.style.display = 'block';
+        }
+        btnJS.onclick = function() {
+            hideElementsMenu();
+            ulJS.style.display = 'block';
+        }
+        btn1c.onclick = function() {
+            hideElementsMenu();
+            ul1c.style.display = 'block';
+        }
 
-    btnPy.onclick = function() {
         hideElementsMenu();
-        ulPy.hidden = true;
-    }
-    btnJS.onclick = function() {
-        hideElementsMenu();
-        ulJS.hidden = true;
-    }
-    btn1c.onclick = function() {
-        hideElementsMenu();
-        ul1c.hidden = true;
-    }
-
-    hideElementsMenu();
-
+    */
 }, false);
