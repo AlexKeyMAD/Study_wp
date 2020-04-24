e6d656a4a92b6c9ba190ddf987792e3fe070289d
Alexandr Gussakov <it_np@mail.ru>
2020-04-25 00:46:10 +0600

    200425_0046

--- less_Home_Library/files/js/menu.js
+++ less_Home_Library/files/js/menu.js
@@ -2,12 +2,12 @@ window.addEventListener('DOMContentLoaded', function() {
     let arr_a = document.getElementsByTagName('a'),
         arr_pages = [];
 
-    for (let index = 0; index < arr_a.length; index++) {
-        let arr_atr = arr_a[index].getAttributeNames();
+    for (let i = 0; i < arr_a.length; i++) {
+        let arr_atr = arr_a[i].getAttributeNames();
         if (arr_atr.indexOf('id') != -1) {
             /*let id = arr_a[index].id,
                 elem = document.getElementById('content');*/
-            arr_pages.push(arr_a[index]);
+            arr_pages.push(arr_a[i]);
             console.log(arr_pages);
             /*elem.src = 'files/content/' + id + '.html';*/
         }
